a7b4c595e15edce01a5f589320c72d18f579c4b1
aashouwang <shouwangzhejuan@yahoo.cn>
2015-07-13 23:52:53 +0800

    ＃扩充画线的方法：

--- TTSAppTemplate/General/Views/CALineView.h
+++ TTSAppTemplate/General/Views/CALineView.h
@@ -13,7 +13,18 @@ typedef NS_ENUM(NSUInteger, TTSLineType) {
     TTSRealLine,        //实线
 };
 
+typedef NS_ENUM(NSUInteger, TTSLinePosition){
+    TTSLinePositionTop = 1,                 //最上面
+    TTSLinePositionLeft,                    //左侧
+    TTSLinePositionBottom,                  //下面
+    TTSLinePositionRight                    //右侧
+};
+
 @interface CALineView : UIView
 @property (nonatomic, retain)UIColor * lineColor;
 @property (nonatomic, assign)TTSLineType lineType;
+@property (nonatomic, assign)CGFloat lineWidth;
+@property (nonatomic, assign)TTSLinePosition linePosition;
+
+- (CALineView *)initWithFrame:(CGRect)frame andLinePosi:(TTSLinePosition)linePosition;
 @end
--- TTSAppTemplate/General/Views/CALineView.m
+++ TTSAppTemplate/General/Views/CALineView.m
@@ -17,11 +17,27 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.opaque = NO;
-        _lineType = TTSDotLine;
-        _lineColor = [UIColor redColor];
+        [self initConstantData];
+        _linePosition = TTSLinePositionTop;
     }
     return self;
+}
+
+- (CALineView *)initWithFrame:(CGRect)frame andLinePosi:(TTSLinePosition)linePosition
+{
+    if (self = [super initWithFrame:frame]) {
+        [self initConstantData];
+        _linePosition = linePosition;
+    }
+    return self;
+}
+
+- (void)initConstantData
+{
+    self.opaque = NO;
+    _lineType = TTSDotLine;
+    _lineColor = [UIColor redColor];
+    _lineWidth = 0.5f;
 }
 
 // Only override drawRect: if you perform custom drawing.
@@ -32,27 +48,43 @@
     
     CGContextBeginPath(context);
     
-    CGContextSetLineWidth(context,0.5);//线宽度
+    CGContextSetLineWidth(context,_lineWidth);//线宽度
     
     CGContextSetStrokeColorWithColor(context, _lineColor.CGColor);
     
     if (_lineType == TTSDotLine) {
         
-        CGFloat lengths[] = {4,2};//先画4个点再画2个点
+        CGFloat lengths[] = {4, 2};//先画4个点再画2个点
         
-        CGContextSetLineDash(context,0, lengths,2);//注意2(count)的值等于lengths数组的长度
+        CGContextSetLineDash(context, 0, lengths, 2);//注意2(count)的值等于lengths数组的长度
     }
     else
     {
-        CGFloat lengths[] = {4,0};//先画4个点再画2个点
+        CGFloat lengths[] = {4, 0};//先画4个点再画2个点
         
-        CGContextSetLineDash(context,0, lengths,2);//注意2(count)的值等于lengths数组的长度
+        CGContextSetLineDash(context, 0, lengths, 2);//注意2(count)的值等于lengths数组的长度
     }
     
-    
-    CGContextMoveToPoint(context,0,0);
-    
-    CGContextAddLineToPoint(context, rect.size.width, 0);
+    //设置横线的位置
+    if (_linePosition == TTSLinePositionTop) {
+        CGContextMoveToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, rect.size.width, 0);
+    }
+    else if (_linePosition == TTSLinePositionLeft)
+    {
+        CGContextMoveToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, rect.size.height);
+    }
+    else if (_linePosition == TTSLinePositionBottom)
+    {
+        CGContextMoveToPoint(context, 0, rect.size.height - _lineWidth);
+        CGContextAddLineToPoint(context, rect.size.width, rect.size.height - _lineWidth);
+    }
+    else if (_linePosition == TTSLinePositionRight)
+    {
+        CGContextMoveToPoint(context, rect.size.width - _lineWidth, 0);
+        CGContextAddLineToPoint(context, rect.size.width - _lineWidth, rect.size.height);
+    }
     
     CGContextStrokePath(context);
     
@@ -74,5 +106,9 @@
     [self setNeedsDisplay];
 }
 
-
+- (void)setLineWidth:(CGFloat)lineWidth
+{
+    _lineWidth = lineWidth;
+    [self setNeedsDisplay];
+}
 @end
